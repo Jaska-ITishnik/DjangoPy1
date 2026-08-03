4e4aac87fd1ff197f6b4e221baaecc0ab4f62331
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2026-08-03 17:47:57 +0500

    function based view completed

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 
 # Create your views here.
 
 def profiles(request):
-    return TemplateResponse(request, template="user_profiles.html")
+    people = {
+        "1": {
+            "fullname": "Botirjon Aliyev",
+            "profession": "Developer"
+        },
+        "2": {
+            "fullname": "Botirjon Aliyev",
+            "profession": "Developer"
+        },
+        "3": {
+            "fullname": "Botirjon Aliyev",
+            "profession": "Developer"
+        }
+    }
+    context = {
+        "people": people
+    }
+    return TemplateResponse(request, template="user_profiles.html", context=context)
